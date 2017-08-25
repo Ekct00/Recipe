4574513b62797ce174ce4b4dc80492b866d99eff
吴世杭 <wushihang@alwaynaive.lan>
2017-08-25 23:29:31 +0800

    Initial Commit

---
+++ recipe1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  recipe1
+//
+//  Created by 吴世杭 on 25/08/2017.
+//  Copyright © 2017 吴世杭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
